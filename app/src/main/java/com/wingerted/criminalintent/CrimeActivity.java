e4dbb0eb3ad1a0f4143cb482f38373298360d270
Winger <wingerted@gmail.com>
2014-09-21 19:58:54 +0800

    The First Commit

--- app/src/main/java/com/wingerted/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/wingerted/criminalintent/CrimeActivity.java
@@ -31,7 +31,7 @@ public class CrimeActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.crime, menu);
-        return true;
+        return false;
     }
 
     @Override
