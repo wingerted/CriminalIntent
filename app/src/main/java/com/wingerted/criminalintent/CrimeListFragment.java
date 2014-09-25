6f8561ebb826026660a19d226132308325e54b0b
Wingerted <wingerted@gmail.com>
2014-09-25 13:56:41 +0800

    Add an list elements

--- app/src/main/java/com/wingerted/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/wingerted/criminalintent/CrimeListFragment.java
@@ -2,6 +2,7 @@ package com.wingerted.criminalintent;
 
 import android.app.ListFragment;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
 
@@ -16,6 +17,13 @@ public class CrimeListFragment extends ListFragment {
         super.onCreate(savedInstanceState);
         getActivity().setTitle(R.string.crime_title);
         mCrimes = CrimeLab.get(getActivity()).getCrimes();
+
+        ArrayAdapter<Crime> adapter =
+                new ArrayAdapter<Crime>(getActivity(),
+                        android.R.layout.simple_list_item_1,
+                        mCrimes);
+
+        setListAdapter(adapter);
     }
 
 }
